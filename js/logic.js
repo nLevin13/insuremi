6d1a8b0bf27708ab6c622cfa8e20c03f35048e90
Zoe Cohen <zoecohen01@gmail.com>
2019-07-18 09:48:22 +0300

    turning of the pages, emergency buttons working

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,36 @@
-$(document).ready(function () {
 
-    $(button).click(function () {
-        if ($(":button").name == "phonecall") {
+$(document).ready(function () {
+    $(":button").click(function () {
+    console.log("button click")
+    console.log(this)
+        if (this.id === "phonecall") {
+            console.log("working")
             $("#wrapper-phonecall").css("display", "block");
             setTimeout(function () {
                 $("wrapper-phonecall").css("display", "none"), 50000
             });
-        } else {
-            moveToNextPage();
+        }else if (this.id === "next_01"){
+            window.location.href = 'q2'
+        }else if (this.id === "next_02"){
+            window.location.href = 'q3'
+        }else if (this.id === "next_03"){
+            window.location.href = 'q4'
+        }else if (this.id === "next_04"){
+            window.location.href = 'q5'
         }
+    });
 
 
-    }
-)});
 
-// var moveToNextPage = function(){
-// if($(":button").name == 
-// }
+
+
+
+
+
+
+
+
+   });
+
+
+
