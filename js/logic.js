a7da21c6832692802c534f0a0371ffd9a1245365
cheatdaycamp <jonakrusze@gmail.com>
2019-07-18 10:54:59 +0300

    improvements on pages connectivity

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $(":button").click(function () {
         console.log("button click")
@@ -10,19 +9,35 @@ $(document).ready(function () {
                 $("wrapper-phonecall").css("display", "none"), 50000
             });
         } else if (this.id === "next_01") {
-            window.location.href = 'q2'
+            window.location.href = 'q2.html'
         } else if (this.id === "next_02") {
-            window.location.href = 'q3'
+            window.location.href = 'q3.html'
         } else if (this.id === "next_03") {
-            window.location.href = 'q4'
+            window.location.href = 'q4.html'
         } else if (this.id === "next_04") {
-            window.location.href = 'q5'
+            window.location.href = 'q5.html'
+        } else if (this.id === "next_05") {
+            window.location.href = 'q6.html'
+        } else if (this.id === "next_06") {
+            window.location.href = 'q7.html'
+        } else if (this.id === "next_07") {
+            window.location.href = 'q8.html'
+        } else if (this.id === "next_08") {
+            window.location.href = 'q9.html'
+        } else if (this.id === "next_09") {
+            window.location.href = 'q10.html'
         }
     });
 
-
-
-
+    $('#signUp').click(function () {
+        window.location.href = 'sign_up2.html'
+    });
+    $('#signIn').click(function () {
+        window.location.href = 'sign_in.html'
+    });
+    $('#next_00').click(function () {
+        window.location.href = 'q1.html'
+    });
 
 
 
